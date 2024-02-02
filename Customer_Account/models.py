0faa50515aae2d9ccdb48138c254564775250b18
salmakhaled888 <salmakhaled888@yahoo.com>
2024-02-02 16:08:52 +0200

    updated

--- Customer_Account/models.py
+++ Customer_Account/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from User.models import CustomUser
 # Create your models here.
 class CustomerAccount(models.Model):
+    national_id = models.CharField(null=True, blank=True, max_length=14,default=None)
     user_id = models.OneToOneField(CustomUser,default= None,on_delete=models.CASCADE,verbose_name="User ID")
     first_name = models.CharField(max_length=100, default=None)
     last_name = models.CharField(max_length=100, default=None)
--- Seller_Account/models.py
+++ Seller_Account/models.py
@@ -4,6 +4,7 @@ from User.models import CustomUser
 
 
 class SellerAccount (models.Model):
+    national_id = models.CharField(null= True,blank=True,max_length=14,default=None)
     user_id = models.OneToOneField(CustomUser,default= None,on_delete=models.CASCADE,verbose_name="User ID")
     first_name = models.CharField(max_length = 100, default=None)
     last_name = models.CharField(max_length=100, default=None)

e7f9b94e09ebb46186467a8a0b78b98a9e076c31
salmakhaled888 <salmakhaled888@yahoo.com>
2024-02-02 21:18:15 +0200

    updated

--- Seller_Account/models.py
+++ Seller_Account/models.py
@@ -9,8 +9,6 @@ class SellerAccount (models.Model):
     user_id = models.OneToOneField(CustomUser,default= None,on_delete=models.CASCADE,verbose_name="User ID")
     first_name = models.CharField(max_length = 100, default=None)
     last_name = models.CharField(max_length=100, default=None)
-    business_name = models.TextField(max_length = 50, default=None)
-    business_field = models.TextField(max_length = 50, default=None)
     telephone = models.PositiveIntegerField(default=None)
     created_at = models.DateField(default=None)
     modified_at = models.DateField(auto_now=True)
